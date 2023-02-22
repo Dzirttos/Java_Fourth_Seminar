194a079d21368ed7664ad853ef68013008c0335a
Dizrtt <demonic2003@bk.ru>
2023-02-22 22:20:24 +0200

    Search by login and phone number added.

--- HW_Task_Phonebook.java
+++ HW_Task_Phonebook.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+
+import javax.sql.rowset.spi.SyncResolver;
 
 // 1 Реализовать телефонный справочник
 // HashMap<Login, Список телефонов>
@@ -13,13 +17,15 @@ public class HW_Task_Phonebook {
 
     public static void main(String[] args) {
         System.out.print("\033[H\033[J");
-        phoneBook.put("Гриша", "8962139001");
+        phoneBook.put("GG", "8962139001");
         phoneBook.put("Морфей", "8962139002");
         phoneBook.put("Потапыч", "8962139000");
 
-        
         // System.out.println(
-        //         "Добро пожаловать в телефонную книгу. Пожалуйста, введите соответствующее число меню:\n1 - Вывести на экран телефонную книгу.\n2 - Добавить номер телефона.\n3 - Удалить конкретный номер телефона. \n4 - Найти по Вашему логину телефон");
+        // "Добро пожаловать в телефонную книгу. Пожалуйста, введите соответствующее
+        // число меню:\n1 - Вывести на экран телефонную книгу.\n2 - Добавить номер
+        // телефона.\n3 - Удалить конкретный номер телефона. \n4 - Найти по Вашему
+        // логину телефон");
 
         Scanner userInput = new Scanner(System.in);
 
@@ -30,11 +36,19 @@ public class HW_Task_Phonebook {
         PrintPhonebook();
         System.out.println();
 
-        String c = userInput.nextLine();
+        // String c = userInput.nextLine();
+
+        // deleteByPhoneNumber(c);
+        // System.out.println();
+        // PrintPhonebook();
+
+        String d = userInput.nextLine();
+
+        // System.out.println(FindPhoneNumber(d));
+        // System.out.println(FindLogin(d));
+        System.out.println(gettKey(phoneBook, d));
+
         userInput.close();
-        deleteByPhoneNumber(c);
-        System.out.println();
-        PrintPhonebook();
 
     }
 
@@ -45,6 +59,7 @@ public class HW_Task_Phonebook {
         }
 
     }
+
     private static void addToPhoneBook(String login, String phonenumber) {
         phoneBook.put(login, phonenumber);
     }
@@ -52,5 +67,35 @@ public class HW_Task_Phonebook {
     private static void deleteByPhoneNumber(String phone) {
         phoneBook.values().removeIf(value -> value.contains(phone));
 
-}
+    }
+
+    public static String FindLogin(String login) {
+        String result = phoneBook.get(login);
+        if (result == null)
+            return "\u001B[31mТакой логин не зарегистрирован";
+        return result;
+    }
+
+    public static String[] FindPhoneNumber(String phone) {
+        List<String> result = new ArrayList<String>();
+        for (Map.Entry entry : phoneBook.entrySet()) {
+            if (phone.equalsIgnoreCase((String) entry.getValue())) {
+                result.add((String) entry.getKey());
+            }
+        }
+        if (result.size() == 0)
+            result.add("\u001B[31mУказанный номер телефонв не найден");
+        return result.toArray(new String[0]);
+    }
+
+    public static <Key, Value> Key gettKey(Map<Key, Value> map, Value value) {
+        for (Map.Entry<Key, Value> entry : map.entrySet()) {
+            if (value.equals(entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        System.out.println("\u001B[31mУказанный номер телефона не найден");
+        System.exit(0);
+        return null;
+    }
 }
